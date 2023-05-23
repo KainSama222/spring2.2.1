671947005f8b0910a22cdabb92dca3807b987c2c
MalozenovIgor <kainsamaelis@gmail.com>
2023-05-23 13:10:53 +0200

    Задание 2.2.1

--- 2.2.1. spring_hibernate/src/main/java/hiber/model/User.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/model/User.java
@@ -1,5 +1,7 @@
 package hiber.model;
 
+import com.mysql.cj.jdbc.MysqlDataSource;
+
 import javax.persistence.*;
 
 @Entity
@@ -20,7 +22,11 @@ public class User {
    private String email;
 
    public User() {}
-   
+
+   @OneToOne(cascade = CascadeType.ALL)
+   @JoinColumn(name = "car_id", referencedColumnName = "id")
+   private Car car;
+
    public User(String firstName, String lastName, String email) {
       this.firstName = firstName;
       this.lastName = lastName;
@@ -57,5 +63,24 @@ public class User {
 
    public void setEmail(String email) {
       this.email = email;
+   }
+
+   public Car getCar() {
+      return car;
+   }
+
+   public Car setCar(Car car) {
+      this.car = car;
+      return car;
+   }
+
+   @Override
+   public String toString() {
+      return "User{" +
+              "id = " + id +
+              ", firstName = '" + firstName + '\'' +
+              ", lastName = '" + lastName + '\'' +
+              ", email = '" + email + '\'' +
+              '}';
    }
 }
